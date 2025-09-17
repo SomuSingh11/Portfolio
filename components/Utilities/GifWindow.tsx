de00269440765d513c230f8672db0c895923e730
SOMU SINGH <somusingh0110@gmail.com>
2025-09-17 22:28:14 +0530

    feat: remove unused GIF from GifWindow component

--- components/Utilities/GifWindow.tsx
+++ components/Utilities/GifWindow.tsx
@@ -5,7 +5,6 @@ import { motion } from "framer-motion";
 export default function GifWindow() {
   const gifs = [
     "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExaG00bTN4OHl0a3hjemRta2dwaDR6cW81MXQ3M2pqeTY5cXRobGEwNiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/116n6kcHaFbw3e/giphy.gif", // Pixel art
-
     "https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif", // Cute penguin
     "https://media.giphy.com/media/3oriO0OEd9QIDdllqo/giphy.gif", // Typing cat
     "https://media.giphy.com/media/13HgwGsXF0aiGY/giphy.gif", // Matrix code
